0b165f0b8ec74103b8c02c9579656faad02d410e
Justin Stayton <justin.stayton@gmail.com>
2013-10-23 10:40:38 -0500

    Added absolute URLs so things work when hosted within a directory on GitHub Pages.

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ tinymce.init({
   selector: 'textarea',
   plugins: 'image',
   external_plugins: {
-    'tinyvision': '/lib/tinyvision/build/plugin.min.js'
+    'tinyvision': 'http://monkdev.github.io/tinyvision/lib/tinyvision/build/plugin.min.js'
   },
   height: '300',
   toolbar: 'image',
   menubar: false,
   statusbar: false,
   tinyvision: {
-    source: '/source.json',
+    source: 'http://monkdev.github.io/tinyvision/source.json',
     upload: function () {
       var message = 'While TinyVision purposely doesn\'t provide upload functionality to keep things simple, it does ' +
                     'provide the ability to hook in your own when the "Upload" button is pressed. Or you can disable ' +
